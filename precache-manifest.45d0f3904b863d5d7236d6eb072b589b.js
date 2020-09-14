a10efc42e220d0406fb0fea6e65ffd83736299ad
Dashawvr <dasha.onishchenko@gmail.com>
2020-09-14 23:18:09 +0300

    Updates

---
+++ precache-manifest.45d0f3904b863d5d7236d6eb072b589b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1633006eed950be5a605db6b6fad6d3",
+    "url": "/nerdy-soft/index.html"
+  },
+  {
+    "revision": "ec53cc52c5a157c90ff9",
+    "url": "/nerdy-soft/static/css/main.fae20cfd.chunk.css"
+  },
+  {
+    "revision": "a89be546817cc64b138f",
+    "url": "/nerdy-soft/static/js/2.e3e276c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nerdy-soft/static/js/2.e3e276c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec53cc52c5a157c90ff9",
+    "url": "/nerdy-soft/static/js/main.5323667c.chunk.js"
+  },
+  {
+    "revision": "24d37ea56078cf395db6",
+    "url": "/nerdy-soft/static/js/runtime-main.d24d2baf.js"
+  }
+]);
